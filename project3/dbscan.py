8654ad6ef1cc4d5dc92fe0f5a561f7a22933fc93
mroduin44 <mroduin44@gmail.com>
2022-04-19 22:35:49 -0600

    Update dbscan.py

--- project3/dbscan.py
+++ project3/dbscan.py
@@ -11,3 +11,15 @@
 #Core
 #Border
 #Noise
+
+#Get distance between 2 points
+def distance(mean_point, distance_point):
+    return math.sqrt(sum([(mean_point[index] - distance_point[index]) ** 2 for index in range(len(mean_point))]))
+
+class Point(self):
+    #Point is a tuple of point values
+    def __init__(self, point):
+        self.point = point
+        self.label =  "noise"
+
+    
